cfe0e4ae89ef3341ddfecc64f93c1bfbc7e56426
Marco Antonio <marco-raya@ciencias.unam.mx>
2024-11-02 17:01:07 -0600

    solve git bugs

---
+++ views/src/components/SingUp.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- Sign up -->
+    <div id="Sign up">
+                <h1>Sign up</h1>
+                <form action="#" method="post">
+                    <div class="input-container">
+                        <label>
+                            Username <span class="req">*</span>
+                        </label>
+                        <input type="text" required>
+                    </div>
+                    <div class="input-container">
+                            <label>
+                                Mail <span class="req">*</span>
+                            </label>
+                        <input type="email" required>
+                    </div>
+                    <div class="input-container">
+                        <label>
+                            Password <span class="req">*</span>
+                        </label>
+                        <input type="password" required>
+                    </div>
+
+                    <input type="submit" class="button button-block" value="Send record">
+                </form>
+            </div>
+</template>
+<script></script>
+<style scoped>
+</style>
