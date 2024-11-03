d6a719f817b2e8e69922f3bdf4964442b8ad2ba0
Marco Antonio <marco-raya@ciencias.unam.mx>
2024-11-02 19:01:58 -0600

    database flask-connection running

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from  models import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:userpassword@mysql:3306/mydatabase'
 
-@app.route("/")
-def hello_world():
-    return "<p>que pedo cachorross</p>"
+# init db instance with the current app context
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- flask/models/users.py
+++ flask/models/users.py
@@ -5,7 +5,7 @@ class Usuarios(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String, nullable=False)
-    authenticated = db.Column(db.Bool, nullable=False)
+    authenticated = db.Column(db.Boolean, nullable=False)
     mail = db.Column(db.String(100), unique=True, nullable=False)
 
     def __repr__(self):
