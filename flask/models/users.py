8b2bd032fe2e5a429c74e6bdbccda8e23b4f0258
Marco Antonio <marco-raya@ciencias.unam.mx>
2024-11-03 20:24:26 -0600

    remove mysql volume persist, add time script config

--- flask/models/users.py
+++ flask/models/users.py
@@ -4,9 +4,15 @@ class Usuarios(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)  # Especifica el tamaño para password_hash
+    password_hash = db.Column(db.String(256), nullable=False)  # Especifica el tamaño para password_hash
     authenticated = db.Column(db.Boolean, nullable=False)
     mail = db.Column(db.String(100), unique=True, nullable=False)
+
+    def get_password_hash(self):
+        return self.password_hash
+    
+    def get_username(self):
+        return self.username
 
     def __repr__(self):
         return f'<Usuario {self.username}>'
