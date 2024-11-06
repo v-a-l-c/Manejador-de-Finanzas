16d20be51ddd0299c38033d3b6b24ac34f710716
Val <val.cb@ciencias.unam.mx>
2024-11-06 14:35:40 -0600

    Base de datos acepta uno o más tags en Transactions

--- flask/models/tags.py
+++ flask/models/tags.py
@@ -1,9 +1,12 @@
 from . import db
+from sqlalchemy.orm import relationship
+from .tag_associations import transaction_tag
 
 class Tags(db.Model):
     __tablename__ = 'tags'
     id = db.Column(db.Integer, primary_key=True)
     tag_name = db.Column(db.String(10), unique=True, nullable=False)
+    registros = relationship('Registro', secondary=transaction_tag, back_populates='tags')
 
     def __repr__(self):
         return f'<Tag {self.tag_name}>'
