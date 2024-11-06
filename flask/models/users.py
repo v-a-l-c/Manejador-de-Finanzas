f2284ce5dcaa09ae597a51c7741fab3dfa3c52ef
Val <val.cb@ciencias.unam.mx>
2024-11-06 13:18:59 -0600

    datos modificables en modelo users

--- flask/models/users.py
+++ flask/models/users.py
@@ -5,9 +5,15 @@ class Usuarios(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)  # Especifica el tamaño para password_hash
+    password_hash = db.Column(db.String(256), nullable=False)
     authenticated = db.Column(db.Boolean, nullable=False)
     mail = db.Column(db.String(100), unique=True, nullable=False)
+
+    #Modificables por el usuario:
+    firstName = db.Column(db.String(50), nullable=False)
+    secondNames = db.Column(db.String(70))
+    curp = db.Column(db.String(18))
+    rfc = db.Column(db.String(13))
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
