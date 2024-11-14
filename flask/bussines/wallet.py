64a91398f4711ca86653a81651051fe6457dec95
Marco Antonio <marco-raya@ciencias.unam.mx>
2024-11-13 21:06:37 -0600

    add incomes insert, show data per day

--- flask/bussines/wallet.py
+++ flask/bussines/wallet.py
@@ -1,6 +1,7 @@
 from models import db
 from models.users import Usuarios
 from models.transactions import Transactions
+from flask import jsonify
 
 """Create a new wallet from the current user. post and put incomes filter by day, month, year to show
 The class only has a logic personal wallet, to insert and show the amount registered. """
@@ -23,7 +24,18 @@ class Wallet:
         db.session.commit()
     
     def get_amount_per_day(self, date):
-        pass
+        show_data = {}
+
+        current_amount = db.session.execute(db.select(Usuarios, Transactions.amount, Transactions.description, Transactions.date)
+        .filter(Usuarios.id == self.user_id)
+        .filter(Transactions.date == date))
+
+        cont = 0
+        for row in current_amount:
+            show_data[cont] = {"amount": row.amount, "description": row.description, "date": row.date}
+            cont += 1
+
+        return show_data
     
     def get_amount_per_month(self, data):
         pass
--- flask/routes/incomes.py
+++ flask/routes/incomes.py
@@ -4,26 +4,36 @@ from bussines.wallet import Wallet
 
 incomes = Blueprint('incomes', __name__)
 
+
+# Define incomes API route.
+# Recieve a data_json with the next keys; amount, description, date and create a current 
+# wallet where the instance recieve a user_id current session.
 @incomes.route('/incomes', methods=['POST'])
 def register_income():
     try:
 
         data_json = request.get_json()
-        #user_id = current_session.get('user_id')
-
-        #if not user:
-         #   return jsonify({"message": "no_user_founded", "response" : str(e)}), 400
-
-        current_wallet = Wallet(1)
+        user_id = current_session.get('user_id')
+        current_wallet = Wallet(user_id)
         current_wallet.insert_amount(data_json.get('amount'), data_json.get('description'), data_json.get('date'))
         return jsonify({"messege": "income_saved_successfully", "response": "success"}), 201
 
     except Exception as e:
         return jsonify({"message" : "server_not_process_data", "response" : str(e)}), 500
 
+# Define incomes API amount per-day route.
+# Recieve a data_json with the next keys; date. it ensures the current session through user_id 
+# hash table reference, therefore it will do current wallet instance.
 @incomes.route('/incomes/per-day', methods=['POST'])
 def get_amount_per_day():
-    pass
+    try:
+        data_json = request.get_json()
+        user_id = current_session.get('user_id')
+        current_wallet = Wallet(user_id)
+        return jsonify({"message": "income_per_day_returned", "resource" : current_wallet.get_amount_per_day(data_json['date'])}), 201
+
+    except Exception as e:
+        return jsonify({"message": "server_not_process_data", "response": str(e)}), 500
 
 @incomes.route('/incomes/per-month', methods=['POST'])
 def get_amount_per_month():
