7be53eaf43bcd2157537b9985d70310b9990010e
Valentina <val.cb@ciencias.unam.mx>
2024-12-10 10:07:57 -0600

    pdf headers

--- flask/bussines/pdf_report.py
+++ flask/bussines/pdf_report.py
@@ -1,17 +1,23 @@
 from fpdf import FPDF
+from datetime import datetime
 
 
-class PDFreport:
+class PDFreport(FPDF):
 
     def __init__(self):
-        pdf = FPDF(orientation = 'P', unit= 'mm', format='A4')
-        pdf.add_page()
-        self.pdf = pdf
-    
+        super().__init__(orientation='P', unit='mm', format='A4')
+        self.add_page()
+
     #title and date
-    def headers(self):
-        pass
-    
+    def header(self):
+        self.set_font('Times', 'B', 15)
+        self.cell(80)
+        self.cell(30, 10, 'Reporte Programado MonkeyApp', 0, 0, 'C')
+        self.ln(10)
+        self.set_font('Times', '', 11)
+        self.cell(0, 10, datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 0, 1, 'R')
+        self.ln(10)
+
     #incomes and expenses table
     def tables_content(self):
         pass
@@ -19,7 +25,6 @@ class PDFreport:
     #balance table
     def balance_content(self):
         pass
-    
+
     def generate_pdf(self):
         self.pdf.output('balance_report.pdf')
-    
